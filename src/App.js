d1b5378af10307b1fa2e0bb4f385211c672a303e
Mohammad815 <mohammadforhad176@gmail.com>
2021-10-03 23:40:11 +0600

    add router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router,Switch,Route } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
     <div className="App">
-      <h1>hellow </h1>
+      <Router>
+        <Switch>
+          <Route exact path="/home">
+            <Home></Home>
+          </Route>
+          <Route exact path="/about">
+            <About></About>
+          </Route>
+          <Route exact path="*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </Router>
+
     </div>
   );
 }
---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button} from 'react-bootstrap';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>I am Home</h1>
+            <Button  variant="success">Button #3</Button>
+        </div>
+    );
+};
+
+export default Home;
